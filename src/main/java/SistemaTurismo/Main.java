22a4165a11e76d45723e8828ddd5d7dfcd266b16
miguelgarbo <miguelggarbo@gmail.com>
2025-04-13 20:16:05 -0300

    Limpando Comentario

--- src/main/java/SistemaTurismo/Main.java
+++ src/main/java/SistemaTurismo/Main.java
@@ -8,49 +8,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//
-//        PasseioEntity passeio = passeioRepository.findById(1);
-//        System.out.println(passeio.toString());
-//
-//        UsuarioEntity usuario = usuarioRepository.findById(6L); //usuario: miguel
-//        UsuarioEntity usuarioCadastradoAoBanco = new UsuarioEntity();
-//
-//        usuarioCadastradoAoBanco.setnome("Joana");
-//        usuarioCadastradoAoBanco.setemail("joana@gmail.com");
-//        usuarioCadastradoAoBanco.setsenha("Joana123#");
-//        usuarioCadastradoAoBanco.setnumeroTelefone("45 98284841439");
-//
-//        usuarioRepository.cadastrar(usuarioCadastradoAoBanco);
-//
-//        System.out.println(usuario.getnome());
-//        System.out.println(usuario.getnumeroTelefone());
-//
-//       List<UsuarioEntity> usuarios = usuarioRepository.buscarTodos();
-//        for(UsuarioEntity u : usuarios){
-//
-//            System.out.println(u.getnome());
-//        }
-//
-//        System.out.println("Achei  eu:  " + usuarioRepository.buscarPorNomeInicial("Miguel"));
-//
-//        UsuarioEntity usuario3 = usuarioRepository.findById(3);
-//
-//        usuario3.setnome("Luciana");
-//        usuario3.setsenha("Luci123@");
-//        usuarioRepository.atualizar(usuario3);
-
-//        UsuarioEntity usuario5 = usuarioRepository.findById(5);
-//
-//        usuarioRepository.remover(usuario5);
-
-        //TESTES COM ROTEIRO_PASSEIOS
-
-//       List<Roteiro_PasseiosEntity> listaPasseiosPorIdRoteiro = roteiroPasseiosRepository.buscarPorIdRoteiro(1);
-//
-//        for(int i =0;i<listaPasseiosPorIdRoteiro.size();i++){
-//
-//            System.out.println(listaPasseiosPorIdRoteiro.get(i).toString());
-//        }
 
         EntityManager em = CustomizerFactory.getEntityManager();
 
